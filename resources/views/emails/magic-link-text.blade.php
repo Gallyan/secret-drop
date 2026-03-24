349afe77a45cb79a3e3827cb63f83fccb12142c3
Guillaume Orsal <guillaume@orsal.net>
2026-03-24 18:04:32 +0100

    bugfix email plain texte

--- resources/views/emails/magic-link-text.blade.php
+++ resources/views/emails/magic-link-text.blade.php
@@ -1,8 +1,8 @@
-{{ __('messages.email_magic_link_intro') }}
+{!! __('messages.email_magic_link_intro') !!}
 
-{{ __('messages.email_magic_link_button') }}: {{ $verifyUrl }}
+{!! __('messages.email_magic_link_button') !!}: {{ $verifyUrl }}
 
-{{ __('messages.email_magic_link_warning') }}
+{!! __('messages.email_magic_link_warning') !!}
 
 --
-{{ __('messages.email_footer', ['app' => config('app.name')]) }}
+{!! __('messages.email_footer', ['app' => config('app.name')]) !!}
