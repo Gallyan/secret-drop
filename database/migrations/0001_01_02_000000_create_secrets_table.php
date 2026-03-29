95f0590900ba0777e96e946fc491ed536daff1f3
Guillaume Orsal <guillaume@orsal.net>
2026-03-30 01:43:52 +0200

    pint

--- database/migrations/0001_01_02_000000_create_secrets_table.php
+++ database/migrations/0001_01_02_000000_create_secrets_table.php
@@ -73,7 +73,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration {
+return new class () extends Migration {
     public function up(): void
     {
         Schema::create('secrets', function (Blueprint $table) {
