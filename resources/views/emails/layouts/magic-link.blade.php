e93b307f228ef094e8461dc63bacaf3ca1df634a
Guillaume Orsal <guillaume@orsal.net>
2026-04-12 10:54:12 +0200

    Fix email footer

--- resources/views/emails/layouts/magic-link.blade.php
+++ resources/views/emails/layouts/magic-link.blade.php
@@ -257,7 +257,7 @@
 
         <div class="footer">
             <p>{{ __('messages.email_footer', ['app' => config('app.name')]) }}</p>
-            <p class="footer-brand">&copy; 2026 <a href="{{ url('/') }}">{{ config('legal.editor_name') }}</a></p>
+            <p class="footer-brand">&copy; 2026 <a href="{{ url('/') }}">{{ config('app.name') }}</a></p>
         </div>
     </div>
 </body>
