4fbacb41c78a45c6477d38d8269a0fc5660ff561
Guillaume Orsal <guillaume@orsal.net>
2026-09-16 11:26:25 +0200

    Affiche le focus clavier sur le sélecteur de thème

--- resources/views/components/theme-toggle.blade.php
+++ resources/views/components/theme-toggle.blade.php
@@ -4,6 +4,7 @@
            bg-linear-to-b from-sky-400 to-blue-600 dark:from-[#0f1b3a] dark:to-[#050b17]
            shadow-[inset_0_2px_6px_rgba(255,255,255,0.42),inset_0_-6px_12px_rgba(0,0,0,0.25),0_8px_14px_rgba(0,0,0,0.18)]
            ring-1 ring-transparent dark:ring-white/10
+           has-focus-visible:outline-2 has-focus-visible:outline-offset-2 has-focus-visible:outline-violet-500
            transition-[background,box-shadow] duration-700 motion-reduce:transition-none"
 >
     <input
